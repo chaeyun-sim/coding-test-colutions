08d4471607f39860b6acd6d975aace54277e51be
chaeyun-sim <bysimune@gmail.com>
2023-02-01 15:21:34 +0900

    initial commit

---
+++ BOJ/Bronze🥉/1271/main.py
@@ -0,0 +1,6 @@
+my_money, people = input().split()
+my_money = int(my_money)
+people = int(people)
+
+print(my_money // people)
+print(my_money % people)
---
+++ BOJ/Bronze🥉/8393/main.py
@@ -0,0 +1,7 @@
+n = input()
+n = int(n)
+
+total = 0
+for i in range(1, n + 1):
+	total += i
+print(total)
